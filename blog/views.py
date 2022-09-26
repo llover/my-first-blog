381e9d3061c849b02df260251bcd439e005349c4
lluvia <sglluvia2@gmail.com>
2022-09-26 16:41:59 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from multiprocessing import reduction
 from django.shortcuts import render
 
+
+def post_list(request):
+  return render(request, 'blog/post_list.html', {})
 # Create your views here.
